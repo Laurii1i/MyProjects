d89cdef326b6b1b73aa97b6385f0942a16a89d93
Jan Härkönen <jan.harkonen@gmail.com>
2024-07-05 15:00:36 +0300

    @0009 Lisää DefaultTranslator

---
+++ GlobalElements/Translators.py
@@ -0,0 +1,39 @@
+import pandas as pd
+from GlobalElements.Languages import Lang
+
+class DefaultTranslator:
+
+
+    def __init__(self, lang : Lang):
+
+
+        assert isinstance(lang, Lang), "Expected 'lang' to be an instance of 'Lang' enumeration ERROR: #0001"
+        self.df = self._create_data_frame()
+        self.default_lang = lang
+    
+    def _create_data_frame(self):
+        
+        dictionary = {'STR_UI_PAIVITA_TIETOKANTA': ['Päivitä tietokanta', 'Uppdatera databas', 'Update database'],
+                      'STR_UI_ETSI_TUOTTEITA': ['Etsi Tuotteita', 'Sök for producter', 'Search products'],
+                      'STR_UI_LUO_TARJOUS': ['Luo Tarjous', 'Skapa erbjudande', 'Create offer'],
+                      'STR_UI_PAIVITA': ['Päivitä', '', ''], #ei enää jaksanu kääntää, laitetaan ku tarvitaan
+                      'STR_UI_HAE_TUOTTEET': ['Hae Tuotteet', '', ''],
+                      'STR_UI_LISAA_KORIIN': ['Lisää koriin', '', ''],
+                      'STR_UI_UUSI_KORI': ['Uusi kori', '', ''],
+                      'STR_UI_ASETA_ALENNUS': ['Aseta alennus', '', ''],
+                      'STR_UI_TALLENNA_SESSIO': ['Tallenna sessio', '', ''],
+                      'STR_UI_TALLENNA_KUVAUS': ['Tallenna kuvaus', '', '']}
+        languages = ['FI', 'SV', 'EN']
+        return pd.DataFrame(data=dictionary, index=languages).T
+        
+
+    def get_string(self, keyword: str, lang: Lang = None) -> str:
+        if lang == None:
+            lang = self.default_lang
+
+        series = self.df.iloc[:,lang]
+        str_out = series.loc[keyword]
+        if str_out != '':
+            return str_out
+        else: 
+            return '['+keyword+']'
--- tests/test_1.py
+++ tests/test_1.py
@@ -5,6 +5,7 @@ import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from GlobalElements.Languages import Lang
+from GlobalElements.Translators import *
 
 class TestDataTransformation(unittest.TestCase):
 
@@ -17,6 +18,39 @@ class TestDataTransformation(unittest.TestCase):
         self.assertEqual(Lang.SV, 1)
         self.assertEqual(Lang.EN, 2)
 
+    def test_DefaultTranslator(self):
+        ##CORRECT DATA
+        default_translator = DefaultTranslator(Lang.FI)
+        self.assertEqual(default_translator.get_string('STR_UI_PAIVITA_TIETOKANTA', Lang.FI), 
+                         'Päivitä tietokanta')
+        self.assertEqual(default_translator.get_string('STR_UI_PAIVITA_TIETOKANTA', Lang.EN), 
+                         'Update database')
+        self.assertEqual(default_translator.get_string('STR_UI_LUO_TARJOUS', Lang.SV), 
+                         'Skapa erbjudande')
+        
+        self.assertEqual(default_translator.get_string('STR_UI_PAIVITA_TIETOKANTA'), 
+                         'Päivitä tietokanta')
+
+        ##INCORRECT DATA
+        self.assertEqual(default_translator.default_lang, Lang.FI)
+        with self.assertRaises(AssertionError):
+            self.assertEqual(default_translator.default_lang, Lang.SV)
+        with self.assertRaises(AssertionError):
+            self.assertEqual(default_translator.default_lang, Lang.EN)
+        
+        with self.assertRaises(AssertionError) as context1:
+            default_translator2 = DefaultTranslator(0)
+        self.assertEqual("Expected 'lang' to be an instance of 'Lang' enumeration ERROR: #0001", str(context1.exception))
+
+        ##MISSING DATA
+        default_translator3 = DefaultTranslator(Lang.FI)
+        dictionary = {'STR_UI_PAIVITA_TIETOKANTA': ['Päivitä tietokanta', 'Uppdatera databas', 'Update database'],
+                        'STR_UI_ETSI_TUOTTEITA': ['Etsi Tuotteita', 'Sök for producter', 'Search products'],
+                        'STR_UI_LUO_TARJOUS': ['Luo Tarjous', '', 'Create offer']} #Pointti on se, että tämä on tyhjä ruotsinkielisessä käännöksessä
+        languages = ['FI','SV','EN']
+        default_translator3.df = pd.DataFrame(data=dictionary, index=languages).T
+        self.assertEqual(default_translator3.get_string('STR_UI_LUO_TARJOUS', Lang.SV), 
+                         '[STR_UI_LUO_TARJOUS]')
 
 if __name__ == '__main__':
     unittest.main()
