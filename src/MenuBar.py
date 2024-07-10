17248b7dd242aeeb18e1c3ee2d0d614f59fd9e3c
Jan Härkönen <jan.harkonen@gmail.com>
2024-07-10 20:43:26 +0300

    lint

--- src/MenuBar.py
+++ src/MenuBar.py
@@ -17,8 +17,12 @@ class MenuBar():
         self.menu_frame = ctk.CTkFrame(root, height = menu_bar_rel_h * window_height, width = 1*window_width)
         self.menu_frame.pack()
 
-        self.update_button = ctk.CTkButton(self.menu_frame, text = Translator.get_string('STR_UI_PAIVITA_TIETOKANTA'), font = (menu_bar_font, menu_bar_font_size), width = menu_bar_button_width)
-        self.update_button.grid(row = 0, column = 0, padx = menu_bar_button_pad)
+        self.update_button = ctk.CTkButton(self.menu_frame,
+                                           text = Translator.get_string('STR_UI_PAIVITA_TIETOKANTA'),
+                                           font = (menu_bar_font, menu_bar_font_size), width = menu_bar_button_width)
+        self.update_button.grid(row = 0,
+                                column = 0,
+                                padx = menu_bar_button_pad)
 
         self.search_button = ctk.CTkButton(self.menu_frame, text = Translator.get_string('STR_UI_ETSI_TUOTTEITA'), font = (menu_bar_font, menu_bar_font_size), width = menu_bar_button_width)
         self.search_button.grid(row = 0, column = 1, padx = menu_bar_button_pad)
